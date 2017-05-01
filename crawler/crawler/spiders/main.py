32f2b1d7509d6b59c2842174ef8d80538f20e5b6
Austin <austinpahl@gmail.com>
2017-04-30 23:04:58 -0400

    Finished main crawler

--- crawler/crawler/spiders/main.py
+++ crawler/crawler/spiders/main.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
-from urlparse import urlparse
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.util import trigrams
+
+from urllib.parse import urlparse
 
 class MainSpider(CrawlSpider):
     name = 'main'
-    start_urls = ['http://google.com/']
+    start_urls = ['https://moz.com/top500']
+    trigram_counts = dict()
+    minimum_trigram_count = 500
 
     rules = (
         Rule(LinkExtractor(), callback='parse_full_text', follow=True),
@@ -13,10 +20,25 @@ class MainSpider(CrawlSpider):
 
     def parse_full_text(self, response):
         domain = self.get_domain(response.url)
-        full_text = ' '.join(sel.select("//body//text()").extract()).strip()
+
+        # if domain is finished, just return 
+        if self.trigram_counts.get(domain, 0) > 500:
+            print('Sufficient data for {}, continuing...'.format(domain))
+            return
+
+        # Get the set of trigrams found on the page
+        sel = Selector(response)
+        full_text = ' '.join(sel.xpath("//body//text()").extract()).strip()
+        word_list = self.tokenize(full_text)
+        trigram_set = self.trigram_set(word_list)
+
+        # Add to the trigram counts
+        self.trigram_counts[domain] = self.trigram_counts.get(domain, 0) + len(trigram_set)
+
+        # Save the item
         i = {
             'domain': domain,
-            'full_text': full_text
+            'trigram_set': trigram_set
         }
         return i
 
@@ -25,3 +47,23 @@ class MainSpider(CrawlSpider):
         parsed_uri = urlparse(url)
         domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
         return domain
+
+    # Source: http://stackoverflow.com/a/15555162
+    def tokenize(self, full_text):
+        tokenizer = RegexpTokenizer(r'\w+')
+        word_set = tokenizer.tokenize(full_text)
+        return word_set
+
+    # Returns a set of byte strings where each string is a trigram from the 
+    # input word list. We need byte strings to pass into the MinHash
+    def trigram_set(self, word_list):
+        trigram_tuples = trigrams(word_list)
+        trigram_list = [' '.join(t) for t in trigram_tuples]
+        return set(trigram_list)
+
+    # def closed( self, reason ):
+    #     # will be called when the crawler process ends
+    #     # any code 
+    #     # do something with collected data 
+    #     for i in self.spider_attr: 
+    #         print i
